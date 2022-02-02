91ae43d8bd4b96ae59e677678a60a3e45dddb6f1
Viktor <vivchy79@gmail.com>
2022-02-02 23:12:26 +0300

    type_variable

---
+++ variable_type.php
@@ -0,0 +1,22 @@
+<?php
+echo PHP_INT_MAX;
+
+$a = 'word';
+$b = 12;
+
+if (isset($a)){
+    echo" $a </br>";
+}
+unset($a);
+
+echo  is_int($b);
+// жесткая ссылка
+$c = &$b;
+echo $c;
+// символическая ссылка
+
+$d = 'c';
+echo $$d;
+
+$d = 34;
+echo $$d; // нет %34
