47a1081a1e72a3d1b7669dcc8452dbd0c792397a
Viktor <vivchy79@gmail.com>
2022-02-05 21:34:19 +0300

    form query

--- index.php
+++ index.php
@@ -8,3 +8,10 @@ echo $_SERVER['SERVER_NAME'];
 ?>
 
 <a href="variable_type.php"> ссылка на страницу</a>
+
+<h1>форма</h1>
+<form action="action.php">
+    login <input type="text" name="name">
+    password <input type="text" name="password">
+    send <input type="submit" value="send">
+</form>
--- variable_type.php
+++ variable_type.php
@@ -20,3 +20,20 @@ echo $$d;
 
 // $d = 34;
 // echo $$d; нет %34
+
+// отладочные функции
+
+echo "<pre> var_dump($c)</pre>";
+echo "<pre> print_r($c)</pre>";
+echo var_export($c);
+
+$person = [
+    'name'=> 'Ivan',
+    'surname' => 'Fuckoff'
+];
+
+echo "name is $person[name] \n"; // /n переводит в коде в html выводе перевод через html tags
+echo "surname is {$person['surname']} </br>";
+echo 16 ** 0.5;
+
+
